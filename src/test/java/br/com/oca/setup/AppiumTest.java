4900752ef7994f1584521b97333244930b47efcd
vitchar <vitorchalupperadi@correio.itau.com.br>
2019-11-21 10:16:49 -0300

    New

--- src/test/java/br/com/oca/setup/AppiumTest.java
+++ src/test/java/br/com/oca/setup/AppiumTest.java
@@ -24,7 +24,7 @@ import com.microsoft.appcenter.appium.EnhancedAndroidDriver;
 
 public class AppiumTest {
 
-//    public AndroidDriver driver;
+    public AndroidDriver driver;
   //  public WebDriverWait wait;
   //
 
@@ -37,35 +37,36 @@ public class AppiumTest {
     @Before
     public void setup() throws MalformedURLException {
         
-/*       DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-        desiredCapabilities.setCapability("platformName", "Android");
+     DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+    /*    desiredCapabilities.setCapability("platformName", "Android");
         desiredCapabilities.setCapability("deviceName", "Android_7.0");
         desiredCapabilities.setCapability("automationName", "uiautomator2");
         desiredCapabilities.setCapability("app", "src/test/resources/uy.com.oca.ocatarjetas_25_apps.evozi.com.apk");
         desiredCapabilities.setCapability("udid", "emulator-5554");
         desiredCapabilities.setCapability("fullReset", true);
-        desiredCapabilities.setCapability("avd", "Android_7.0");
+        desiredCapabilities.setCapability("avd", "Android_7.0"); */
+
+
         driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), desiredCapabilities);
-        //AppiumDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://0.0.0.0:4723/wd/hub"), desiredCapabilities);
-	//
-	*/
+        AppiumDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://0.0.0.0:4723/wd/hub"), desiredCapabilities);
+
+
         
-    } 
-  
-      @Test
-      public void calculo_bitcoin_dolarTest() throws MalformedURLException, InterruptedException {
-                
-        Assert.assertEquals("test","test");
-        
-      }
+    }
 
-      @Test
-      public void calculo_bitcoin_pesoTest() throws MalformedURLException, InterruptedException {
+    @Test
+    public void calculo_bitcoin_dolarTest() throws MalformedURLException, InterruptedException {
 
-        Assert.assertEquals("test","test");
+        driver.findElementById("com.livrokotlin.bitcoincalculator:id/txt_amount").sendKeys("15000");
+        driver.findElementById("com.livrokotlin.bitcoincalculator:id/btn_calculate").click();
 
-      }
-    
+        String resultado = driver.findElementById("com.livrokotlin.bitcoincalculator:id/txt_qtd_bitcoins").getText();
+
+        Assert.assertEquals("test", "test");
+
+    }
+
+
     @After
     public void teardown(){
     	
