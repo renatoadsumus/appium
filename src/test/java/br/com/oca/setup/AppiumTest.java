1d427c567b3a4a4abdda1d00b64296d927dfec42
root <root@ip-172-31-47-159.ec2.internal>
2019-11-26 17:19:17 +0000

    Removendo Selenium

--- src/test/java/br/com/oca/setup/AppiumTest.java
+++ src/test/java/br/com/oca/setup/AppiumTest.java
@@ -14,23 +14,33 @@ import java.net.URL;
 
 import org.junit.*;
 
+import java.util.concurrent.TimeUnit;
+
 import org.junit.rules.TestWatcher;
 
 public class AppiumTest {
 
-	public static AndroidDriver driver;
+	//public static AndroidDriver driver;
 	// public WebDriverWait wait;
+	public static AndroidDriver<MobileElement> driver;
 
-	@BeforeClass
-	public static void myTest() throws MalformedURLException {
+	@Before
+        public void Setup() throws MalformedURLException{
+  
+
+		URL url = new URL("http://localhost:4723/wd/hub");
 
 		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-		driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), desiredCapabilities);
+
+		driver = new AndroidDriver<MobileElement>(url, desiredCapabilities);
+
+		driver.manage().timeouts().implicitlyWait(35, TimeUnit.SECONDS);
 	}
 
 	@Test
-	public void calculo_bitcoin_dolarTest() throws MalformedURLException, InterruptedException {
+	public void calculo_bitcoin_dolarNewTest() throws MalformedURLException, InterruptedException {
 		
+
 		driver.findElementById("com.livrokotlin.bitcoincalculator:id/txt_amount").sendKeys("15000");
 		driver.findElementById("com.livrokotlin.bitcoincalculator:id/btn_calculate").click();
 		
