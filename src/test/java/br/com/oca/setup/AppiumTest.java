774e137735cc015d82ed34de95ad3d1bace84050
Javier <jsolis@roshka.com>
2019-12-12 09:39:38 -0300

    Incluindo otros device

--- src/test/java/br/com/oca/setup/AppiumTest.java
+++ src/test/java/br/com/oca/setup/AppiumTest.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
- 
+
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -41,16 +41,28 @@ public class AppiumTest {
     @BeforeTest
     public void setup() throws MalformedURLException {
 
-       DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
         desiredCapabilities.setCapability("platformName", "Android");
-        desiredCapabilities.setCapability("deviceName", "SM-G935F");
-        //desiredCapabilities.setCapability("automationName", "uiautomator2");
+
         desiredCapabilities.setCapability("app", "/home/javier/Escritorio/app-PF-debug.apk");
-        desiredCapabilities.setCapability("udid", "ad08160310e018e1ec");
+
+        //S7 - Android 8
+//        desiredCapabilities.setCapability("udid", "ad08160310e018e1ec");
+//        desiredCapabilities.setCapability("deviceName", "SM-G935F");
+
+        //GRAN PRIME - Android 5
+        desiredCapabilities.setCapability("udid", "f58cce01");
+        desiredCapabilities.setCapability("deviceName", "SM-G530H");
+
+        //A20 - Android 9
+//        desiredCapabilities.setCapability("udid", "R58M83DJ3ZE");
+//        desiredCapabilities.setCapability("deviceName", "SM-A205G");
+
+
         desiredCapabilities.setCapability("fullReset", true);
         //desiredCapabilities.setCapability("avd", "Android_7.0");
         driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), desiredCapabilities);
-       // AppiumDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://0.0.0.0:4723/wd/hub"), desiredCapabilities);
+        // AppiumDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://0.0.0.0:4723/wd/hub"), desiredCapabilities);
 
         //driver.manage().timeouts().implicitlyWait(30, SECONDS);
 
@@ -58,7 +70,7 @@ public class AppiumTest {
 
 
     @Test
-      public void loginASiRecordarTest() throws InterruptedException {
+    public void loginASiRecordarTest() throws InterruptedException {
 
         Thread.sleep(5000);
         driver.findElementById("py.com.itau.nmb.android:id/login_num_doc").sendKeys("298976");
@@ -69,19 +81,18 @@ public class AppiumTest {
         String resultadoEsperado = "recordar documento y cuenta";
 
 
-                
-        Assert.assertEquals(resultadoEsperado,resultadoActual);
-        
-      }
+        Assert.assertEquals(resultadoEsperado, resultadoActual);
+
+    }
 
 
     @Test
     public void loginBConRecordarTest() throws InterruptedException {
 
         Thread.sleep(5000);
-        driver.findElementById("py.com.itau.nmb.android:id/login_num_doc").sendKeys("298976");
-        driver.findElementById("py.com.itau.nmb.android:id/login_num_cuenta").sendKeys("820008260");
-        driver.findElementById("py.com.itau.nmb.android:id/edt_login_pass").sendKeys("15000");
+        driver.findElementById("py.com.itau.nmb.android:id/login_num_doc").sendKeys("3709122");
+        driver.findElementById("py.com.itau.nmb.android:id/login_num_cuenta").sendKeys("702703180");
+        driver.findElementById("py.com.itau.nmb.android:id/edt_login_pass").sendKeys("147258");
 
         driver.findElementById("py.com.itau.nmb.android:id/reminder").click();
 
@@ -90,19 +101,16 @@ public class AppiumTest {
         String resultadoEsperado = "recordar documento y cuenta";
 
 
-
-        Assert.assertEquals(resultadoEsperado,resultadoActual);
+        Assert.assertEquals(resultadoEsperado, resultadoActual);
 
     }
 
 
-
-
-    
     @AfterClass
-    public void teardown(){
-    	
-    	System.out.println("FIM");
+    public void teardown() {
+
+        System.out.println("FIM");
         //driver.quit();
     }
+
 }
